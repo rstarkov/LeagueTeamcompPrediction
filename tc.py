f1d5744112e3759cd92a0c85e46dce277933cf73
rstarkov <sourcecode@roman.st>
2019-01-09 23:21:44 +0000

    Basic training loop. Add mirror matchups with winrate 0.5 to training set.

--- tc.py
+++ tc.py
@@ -8,6 +8,11 @@ np.random.seed(1)
 tf.set_random_seed(1)
 random.seed(1)
 
+def log(val):
+    print("LOG:", val)
+    print(val, file=logfile)
+    logfile.flush()
+
 def read_shr_uint8(filename, cols):
     arrs = []
     blocks = 0
@@ -53,32 +58,78 @@ def read_shr_categories(filename):
       for k, v in reader: d[k] = v
     return d
 
-print("Loading match data...")
-champLabels = read_shr_categories('Data/teamcomp.plr.champ.classes.csv')
-champCount = len(champLabels)
-timestamps = read_shr_uint16('Data/teamcomp.timestamp.shr.gz', 1)
-champs = read_shr_uint8('Data/teamcomp.plr.champ.shr.gz', 10) # 5 winners, followed by 5 losers
-print("Loaded {:,} matches".format(timestamps.shape[0]))
-
-def construct_batch(first, count):
-    # main batch
+def construct_chunk(first, count):
+    log("Constructing chunk...")
+    # main chunk
     last = first + count
-    inputs = np.zeros((count*2, champCount*2 + 1))
-    outputs = np.ones((count*2, 1)) # Did first team win
+    inputs = np.zeros((count*3, champ_count*2 + 1))
+    outputs = np.ones((count*3, 1)) # Did first team win
     for ch in range(5):
         inputs[range(count), champs[first:last, ch]] = 1
     for ch in range(5):
-        inputs[range(count), champCount + champs[first:last, 5+ch]] = 1
+        inputs[range(count), champ_count + champs[first:last, 5+ch]] = 1
     inputs[:count, -1] = timestamps[first:last, 0]
 
     # mirrored
-    inputs[count:, :champCount] = inputs[:count, champCount:(2*champCount)]
-    inputs[count:, champCount:(2*champCount)] = inputs[:count, :champCount]
-    inputs[count:, -1] = inputs[:count, -1]
-    outputs[count:, 0] = 0
+    inputs[count:2*count, :champ_count] = inputs[:count, champ_count:2*champ_count]
+    inputs[count:2*count, champ_count:2*champ_count] = inputs[:count, :champ_count]
+    inputs[count:2*count, -1] = inputs[:count, -1]
+    outputs[count:2*count, 0] = 0
 
+    # equalized (not all of the games)
+    inputs[2*count:, :] = inputs[:count, :]
+    inputs[2*count:, champ_count:2*champ_count] = inputs[2*count:, :champ_count]
+    outputs[2*count:, 0] = 0.5
+
+    log("Chunk constructed")
     return inputs, outputs
 
-test, outputs = construct_batch(0, 10)
-np.savetxt("dump.csv", test, delimiter=",")
-print(outputs)
+logfile = open('log.txt', 'a')
+log("==============================")
+
+log("Loading match data...")
+champ_labels = read_shr_categories('data/teamcomp.plr.champ.classes.csv')
+champ_count = len(champ_labels)
+timestamps = read_shr_uint16('data/teamcomp.timestamp.shr.gz', 1)
+champs = read_shr_uint8('data/teamcomp.plr.champ.shr.gz', 10) # 5 winners, followed by 5 losers
+matchCount = timestamps.shape[0]
+shuf = np.random.permutation(match_count)
+timestamps = timestamps[shuf]
+champs = champs[shuf]
+log("Loaded {:,} matches".format(match_count))
+
+chunk_size = 1000000 # 1 million size = 3 million training samples = 7 GB of RAM
+validation_size = 100000
+inputs, outputs = construct_chunk(0, 10)
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(256, activation=tf.nn.elu, input_shape=(inputs.shape[1],)))
+model.add(keras.layers.Dense(128, activation=tf.nn.elu))
+model.add(keras.layers.Dense(64, activation=tf.nn.elu))
+model.add(keras.layers.Dense(64, activation=tf.nn.elu))
+model.add(keras.layers.Dense(32, activation=tf.nn.elu))
+model.add(keras.layers.Dense(32, activation=tf.nn.elu))
+model.add(keras.layers.Dense(32, activation=tf.nn.elu))
+model.add(keras.layers.Dense(32, activation=tf.nn.elu))
+model.add(keras.layers.Dense(1))
+
+model.summary()
+
+learn_rate = 0.0001
+
+start_time = time.time()
+epoch = 1
+chunks = match_count // chunk_size
+chunk_size = match_count // chunks
+log('Using chunk size {:,}'.format(chunk_size))
+while True:
+    model.compile(loss = 'mse', optimizer = keras.optimizers.RMSprop(learn_rate), metrics = ['mse', 'mae'])
+    for c in range(chunks):
+        log("Training on chunk %d of %d" % (c+1, chunks))
+        inputs, outputs = construct_chunk(c*chunk_size, chunk_size)
+        hist = model.fit(inputs, outputs, epochs=1, batch_size=32, shuffle=True)
+        log(abs(hist.history["loss"][-1] - 0.25))
+        model.save('models/tc-%04d-%03d.h5' % (epoch, ))
+
+    epoch = epoch + 1
+    learn_rate = 0.8 * learn_rate
